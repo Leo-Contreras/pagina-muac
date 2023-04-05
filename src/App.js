7256a7529f8f9b87cebebe46e12d33f4a3f566df
oscar.reyes <oareyes@baja.gob.mx>
2023-04-05 15:57:27 -0700

    Segunda version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {Container, Row, Col, Button, Card} from 'react-bootstrap';
-import logo from './imagenes/logo guinda bc - copia.png';
+import logo from './imagenes/logo adbc png.png';
 import logoMuac from './imagenes/BC-MUAC-mobile@4k - copia.jpg';
 import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube, FaWhatsapp, FaFacebookMessenger } from 'react-icons/fa';
 import './App.css';
+import cardImg1 from './imagenes/card 1 img.png';
+import cardImg2 from './imagenes/card img 2.png';
+import cardImg3 from './imagenes/card img 3.png';
+import cardImg4 from './imagenes/card img 4.png';
+import cardImg5 from './imagenes/card img 5.png';
+
 
 
 
@@ -13,7 +19,9 @@ function App() {
             <Container>
             <Row className="align-items-center my-3">
                 <Col xs="auto">
+                    <a href="https://gestion.bajacalifornia.gob.mx/adbc/#">
                     <img src={logo} alt="Logo" className="logo" />
+                    </a>
                 </Col>
                 <Col className="text-end">
                     <Button className="share-button">Compartir</Button>
@@ -21,14 +29,16 @@ function App() {
             </Row>
             <Row className="my-3">
                 <Col>
-                    <h1 className="header">Atención Ciudadana BC</h1>
+                    <h1 className="header-2" > Modelo Único de</h1>
+                    <h1 className="header">  Atención Ciudadana BC </h1>
                 </Col>
             </Row>
             <Row className="my-3">
                 <Col className="text-justify">
+                    <hr style={{color: "white"}}/>
                     <h6 className="texto-bienvenida">
-                        ¡Bienvenido a nuestra página del Modelo Único de Atención Ciudadana (MUAC)! En el Estado de Baja California, nuestra prioridad es servirte de la mejor manera posible. Nuestro equipo de atención ciudadana está formado por profesionales altamente capacitados y comprometidos con tu bienestar.
-                        Estamos aquí para escucharte, orientarte y resolver cualquier duda o inquietud que puedas tener. No dudes en contactarnos, estamos siempre a tu disposición para ayudarte en todo lo que necesites. ¡Gracias por confiar en nosotros!
+                        Bienvenido al MUAC, donde estamos comprometidos en servirte de la mejor manera. Contamos con un equipo altamente capacitado para atenderte, resolver tus dudas e inquietudes.
+                        No dudes en contactarnos, estamos siempre a tu disposición para ayudarte. ¡Gracias por confiar en nosotros!
                     </h6>
                 </Col>
                 <Col className="text-center">
@@ -42,7 +52,7 @@ function App() {
                 <Container>
                     <Row className="my-5">
                         <Col className="text-center">
-                            <h2 className="contact-header">CONTÁCTANOS</h2>
+                            <h1 className="contact-header">CONTÁCTANOS</h1>
                             <h5 className="contact-subheader">
                                 Comunícate con nosotros y obtén la ayuda que necesitas
                             </h5>
@@ -50,48 +60,50 @@ function App() {
                     </Row>
                     <Row>
                         <Col md={6}>
-                            <Card className="mb-4 card-1">
-                                <Card.Body>
-                                    <Card.Title>Card 1</Card.Title>
-                                    <Card.Text>Descripción del card 1.</Card.Text>
-                                </Card.Body>
-                            </Card>
+                            <img
+                                className="mb-4 custom-image card-1"
+                                src={cardImg1}
+                                alt="Card 1"
+                                onClick={() => window.open('http://www.bajacalifornia.gob.mx', '_blank')}
+                            />
                         </Col>
                         <Col md={3}>
-                            <Card className="mb-4 card-2">
-                                <Card.Body>
-                                    <Card.Title>Card 2</Card.Title>
-                                    <Card.Text>Descripción del card 2.</Card.Text>
-                                </Card.Body>
-                            </Card>
+                            <img
+                                className="mb-4 custom-image card-2"
+                                src={cardImg2}
+                                alt="Card 2"
+                                onClick={() => window.open('https://api.whatsapp.com', '_blank')}
+                            />
                         </Col>
                         <Col md={3}>
-                            <Card className="mb-4 card-3">
-                                <Card.Body>
-                                    <Card.Title>Card 3</Card.Title>
-                                    <Card.Text>Descripción del card 3.</Card.Text>
-                                </Card.Body>
-                            </Card>
+                            <img
+                                className="mb-4 custom-image card-3"
+                                src={cardImg3}
+                                alt="Card 3"
+                                onClick={() => window.open('https://www.messenger.com/login.php?next=https%3A%2F%2Fwww.messenger.com%2Ft%2F120544204635995', '_blank')}
+                            />
                         </Col>
                     </Row>
                     <Row>
                         <Col md={6}>
-                            <Card className="mb-4 card-4">
-                                <Card.Body>
-                                    <Card.Title>Card 4</Card.Title>
-                                    <Card.Text>Descripción del card 4.</Card.Text>
-                                </Card.Body>
-                            </Card>
+                            <img
+                                className="mb-4 custom-image card-4"
+                                src={cardImg4}
+                                alt="Card 4"
+                                onClick={() => window.open('http://sctg.bajacalifornia.gob.mx:8080/quejas/queja.jsp', '_blank')}
+                            />
                         </Col>
                         <Col md={6}>
-                            <Card className="mb-4 card-5">
-                                <Card.Body>
-                                    <Card.Title>Card 5</Card.Title>
-                                    <Card.Text>Descripción del card 5.</Card.Text>
-                                </Card.Body>
-                            </Card>
+                            <img
+                                className="mb-4 custom-image card-5"
+                                src={cardImg5}
+                                alt="Card 5"
+                                onClick={() => window.open('linktr.ee/atencionciudadanabc', '_blank')}
+                            />
                         </Col>
                     </Row>
+
+
                 </Container>
             </div>
 
@@ -103,35 +115,47 @@ function App() {
                     </Col>
                 </Row>
                 <Row>
-                    <Col sm={6} md={3} >
-                        <Card className="mb-4 card-6">
+                        <Col xs={12} sm={6} md={6} lg={3} className="mb-4">
+                            <Card className="card-6" >
+                                <Card.Body>
+                                    <Card.Text ><h1><strong>Citas del Gobierno del Estado</strong></h1></Card.Text>
+                                    <Card.Text >
+                                        Agenda tu cita en la oficina de tu municipio más cercana para realizar el trámite o servicio que necesitas. ¡Te esperamos!
+                                    </Card.Text>
+                                    <Card.Link href="https://citas.ebajacalifornia.gob.mx/" target="_blank">Ir al sitio de Citas</Card.Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col xs={12} sm={6} md={6} lg={3} className="mb-4">
+                            <Card className="card-7" onClick={() => window.open('https://tramites.ebajacalifornia.gob.mx/ventanillaunica/', '_blank')}>
+                                <Card.Body>
+                                    <Card.Title><h1><strong>Trámites Digitales</strong> </h1></Card.Title>
+                                    <Card.Text>
+                                        ¡Te invitamos a descubrir nuestros nuevos servicios en línea! Ahora podrás realizar más servicios desde la comodidad de tu hogar y sin necesidad de hacer filas.
+                                    </Card.Text>
+                                    <Card.Link href="https://tramites.ebajacalifornia.gob.mx/ventanillaunica/" target="_blank">Ir a Ventanilla digital</Card.Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col xs={12} sm={6} md={6} lg={3} className="mb-4">
+                            <Card className="card-8" onClick={() => window.open('http://appsweb.ebajacalifornia.gob.mx/sifos/kiosko/index', '_blank')}>
+                                <Card.Body>
+                                    <Card.Title > <h1><strong>Kioskos Multi-Servicios </strong></h1></Card.Title>
+                                    <Card.Text>
+                                        Son cajeros automáticos que están distribuidos en los cinco municipios de la entidad para que puedas realizar tus trámites y pagos de manera ágil.
+                                    </Card.Text>
+                                    <Card.Link href="http://appsweb.ebajacalifornia.gob.mx/sifos/kiosko/index" target="_blank">Ubica los Kioskos</Card.Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    <Col xs={12} sm={6} md={6} lg={3} className="mb-4">
+                        <Card className="card-9" onClick={() => window.open('https://www.bajacalifornia.gob.mx/Gobierno/FAQ', '_blank')}>
                             <Card.Body>
-                                <Card.Title>Card 6</Card.Title>
-                                <Card.Text>Descripción del card 6.</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col sm={6} md={3}>
-                        <Card className="mb-4 card-7">
-                            <Card.Body>
-                                <Card.Title>Card 7</Card.Title>
-                                <Card.Text>Descripción del card 7.</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col sm={6} md={3}>
-                        <Card className="mb-4 card-8">
-                            <Card.Body>
-                                <Card.Title>Card 8</Card.Title>
-                                <Card.Text>Descripción del card 8.</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col sm={6} md={3}>
-                        <Card className="mb-4 card-9">
-                            <Card.Body>
-                                <Card.Title>Card 9</Card.Title>
-                                <Card.Text>Descripción del card 9.</Card.Text>
+                                <Card.Title> <h1><strong>Preguntas frecuentes</strong> </h1></Card.Title>
+                                <Card.Text>
+                                    Aquí encontrarás las preguntas más frecuentes de los ciudadanos sobre los trámites de Control Vehicular (Placas), Licencias de Conducir e Impuestos que ofrece la Secretaría de Hacienda.
+                                </Card.Text>
+                                <Card.Link href="https://www.bajacalifornia.gob.mx/Gobierno/FAQ" target="_blank">Ir a Preguntas frecuentes</Card.Link>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -144,44 +168,45 @@ function App() {
                                 <h2 className="social-header">Consulta nuestras redes sociales oficiales</h2>
                             </Col>
                         </Row>
-                            <Row className="social-icons">
-                                <Col className="social-icon-col">
-                                    <a href="/" target="_blank" rel="noopener noreferrer">
-                                        <FaFacebookF />
-                                        <p>Facebook</p>
-                                    </a>
-                                </Col>
-                                <Col className="social-icon-col">
-                                    <a href="/" target="_blank" rel="noopener noreferrer">
-                                        <FaInstagram />
-                                        <p>Instagram</p>
-                                    </a>
-                                </Col>
-                                <Col className="social-icon-col">
-                                    <a href="/" target="_blank" rel="noopener noreferrer">
-                                        <FaFacebookMessenger />
-                                        <p>Messenger</p>
-                                    </a>
-                                </Col>
-                                <Col className="social-icon-col">
-                                    <a href="/" target="_blank" rel="noopener noreferrer">
-                                        <FaTwitter />
-                                        <p>Twitter</p>
-                                    </a>
-                                </Col>
-                                <Col className="social-icon-col">
-                                    <a href="/" target="_blank" rel="noopener noreferrer">
-                                        <FaWhatsapp />
-                                        <p>WhatsApp</p>
-                                    </a>
-                                </Col>
-                                <Col className="social-icon-col">
-                                    <a href="/" target="_blank" rel="noopener noreferrer">
-                                        <FaYoutube />
-                                        <p>YouTube</p>
-                                    </a>
-                                </Col>
-                            </Row>
+                        <Row className="social-icons">
+                            <Col className="social-icon-col">
+                                <a href="https://www.facebook.com/AgenciaDigitaldeBajaCalifornia" target="_blank" rel="noopener noreferrer" className="social-icon">
+                                    <FaFacebookF size ="3x" />
+                                    <p>Facebook</p>
+                                </a>
+                            </Col>
+                            <Col className="social-icon-col">
+                                <a href="https://www.instagram.com/BCGobierno/" target="_blank" rel="noopener noreferrer" className="social-icon">
+                                    <FaInstagram size ="3x"/>
+                                    <p>Instagram</p>
+                                </a>
+                            </Col>
+                            <Col className="social-icon-col">
+                                <a href="/" target="_blank" rel="noopener noreferrer" className="social-icon">
+                                    <FaFacebookMessenger size ="3x" />
+                                    <p>Messenger</p>
+                                </a>
+                            </Col>
+                            <Col className="social-icon-col">
+                                <a href="https://twitter.com/bcgobierno" target="_blank" rel="noopener noreferrer" className="social-icon">
+                                    <FaTwitter  size ="3x"/>
+                                    <p>Twitter</p>
+                                </a>
+                            </Col>
+                            <Col className="social-icon-col">
+                                <a href="/" target="_blank" rel="noopener noreferrer" className="social-icon">
+                                    <FaWhatsapp size ="3x"/>
+                                    <p>WhatsApp</p>
+                                </a>
+                            </Col>
+                            <Col className="social-icon-col">
+                                <a href="/" target="_blank" rel="noopener noreferrer" className="social-icon">
+                                    <FaYoutube size ="3x"/>
+                                    <p>YouTube</p>
+                                </a>
+                            </Col>
+                        </Row>
+
                         <Row className="my-5">
                             <Col>
                                 <p className="copyright-text">
